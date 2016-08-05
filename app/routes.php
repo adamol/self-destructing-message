d8f728bd3f789e30571776054797131b8a9e0df3
adamol <adamol1992@gmail.com>
2016-08-05 18:06:00 +0200

    Add ability to store messages in database

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,21 @@
 
 $app->get('/', function($request, $response, $args) {
 	$this->view->render($response, 'home.twig');
-});
+})->setName('home');
+
+$app->post('/', function($request, $response, $args) use ($app) {
+	$params = $request->getParams();
+	$hash = md5(uniqid(true));
+
+	$message = $this->db->prepare("
+		INSERT INTO messages (hash, message)
+		VALUES (:hash, :message)
+	");
+
+	$message->execute([
+		'hash' => $hash,
+		'message' => $params['message']
+	]);
+
+	return $response->withRedirect($this->router->pathFor('home'));
+})->setName('send');
